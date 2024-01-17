87927cb0cb5fb3dcb4a6ec860dbca727c692fd06
Shaikat0079 <baruashaikat00790@gmail.com>
2024-01-17 22:41:21 +0930

    Method

--- main.py
+++ main.py
@@ -9,13 +9,20 @@
 # print(type(item1_price_total)) #int
 
 class Item:
-    pass
+    def calculat_total_price(self,x,y):
+        return x * y
 
 
 item1 = Item()
 item1.name = "Phone"
 item1.price = 100
 item1.quantity = 5
+print(item1.calculat_total_price(item1.price,item1.quantity))
+
+item2 = Item()
+item2.name = "Laptop"
+item2.price = 1000
+item2.quantity = 3
 
 # print(type(item1))
 # print(type(item1.name))
