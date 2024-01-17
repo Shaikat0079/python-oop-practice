247b2c67bf6f68dd9857d8938fb6dcec87644c9c
Shaikat0079 <baruashaikat00790@gmail.com>
2024-01-17 23:57:06 +0930

    Some basic changes

--- main.py
+++ main.py
@@ -9,6 +9,8 @@
 # print(type(item1_price_total)) #int
 
 class Item:
+    def __init__(self):
+        print("I am Created!")
     def calculat_total_price(self,x,y):
         return x * y
 
@@ -17,12 +19,13 @@ item1 = Item()
 item1.name = "Phone"
 item1.price = 100
 item1.quantity = 5
-print(item1.calculat_total_price(item1.price,item1.quantity))
+# print(item1.calculat_total_price(item1.price,item1.quantity))
 
 item2 = Item()
 item2.name = "Laptop"
 item2.price = 1000
 item2.quantity = 3
+# print(item2.calculat_total_price(item2.price,item2.quantity))
 
 # print(type(item1))
 # print(type(item1.name))
