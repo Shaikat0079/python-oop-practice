3bb78c8c11f87c2c947abb76f9ec3232bef68941
Shaikat0079 <baruashaikat00790@gmail.com>
2024-01-17 22:12:04 +0930

    Class name added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ item1_price = 100
 item1_quality = 5
 item1_price_total = item1_price * item1_quality
 
-print(type(item1))
-print(type(item1_price))
-print(type(item1_quality))
-print(type(item1_price_total))
+print(type(item1)) #str
+print(type(item1_price)) #int
+print(type(item1_quality)) #int
+print(type(item1_price_total)) #int
