1611a526acd55953d6d00ccbfcd5ec4e9c3414b5
Shaikat0079 <baruashaikat00790@gmail.com>
2024-01-17 22:09:59 +0930

    Very Basics

---
+++ main.py
@@ -0,0 +1,9 @@
+item1= 'Phone'
+item1_price = 100
+item1_quality = 5
+item1_price_total = item1_price * item1_quality
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quality))
+print(type(item1_price_total))
