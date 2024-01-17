5d301082a0551b1f89670abd9097d014dd0e8ed2
Shaikat0079 <baruashaikat00790@gmail.com>
2024-01-18 00:05:46 +0930

    Instance created

--- main.py
+++ main.py
@@ -9,20 +9,20 @@
 # print(type(item1_price_total)) #int
 
 class Item:
-    def __init__(self):
-        print("I am Created!")
+    def __init__(self,name):
+        print(f"An instance Created:{name}")
     def calculat_total_price(self,x,y):
         return x * y
 
 
-item1 = Item()
-item1.name = "Phone"
+item1 = Item("Phone")
+# item1.name = "Phone"
 item1.price = 100
 item1.quantity = 5
 # print(item1.calculat_total_price(item1.price,item1.quantity))
 
-item2 = Item()
-item2.name = "Laptop"
+item2 = Item("Laptop")
+# item2.name = "Laptop"
 item2.price = 1000
 item2.quantity = 3
 # print(item2.calculat_total_price(item2.price,item2.quantity))
