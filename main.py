d372721087bc9b3ade8d33e17a7f7db8202d7e85
Shaikat0079 <baruashaikat00790@gmail.com>
2024-01-18 13:17:02 +0930

    Attributes class level and instance level

--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 # print(type(item1_price_total)) #int
 
 class Item:
+    pay_rate = 0.8 # The pay rate after 20% dicsount(class attribute)
     def __init__(self,name:str,price:float,quantity = 0):
         # print(f"An instance Created:{name}")
         # Run Validatiions to the received Arguments
@@ -20,22 +21,24 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
-    def calculat_total_price(self):
+    def calculate_total_price(self):
         return self.price * self.quantity
 
+    def apply_discount(self):
+        self.price =  self.price * self.pay_rate
 
 # item1 = Item("Phone",100,-1)
-item1 = Item("Phone",100,5)
+item1 = Item("Phone",100,5) # Class er instance
 # item1.name = "Phone"
 # item1.price = 100
 # item1.quantity = 5
-# print(item1.calculat_total_price(item1.price,item1.quantity))
+# print(item1.calculate_total_price(item1.price,item1.quantity))
 
 item2 = Item("Laptop",1000,3)
 # item2.name = "Laptop"
 # item2.price = 1000
 # item2.quantity = 3
-# print(item2.calculat_total_price(item2.price,item2.quantity))
+# print(item2.calculate_total_price(item2.price,item2.quantity))
 
 
 # print(item1.name)
@@ -55,5 +58,22 @@ item2 = Item("Laptop",1000,3)
 # random_str = "aaa"
 # print(random_str.upper())
 
-print(item1.calculat_total_price())
-print(item2.calculat_total_price())
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
+
+
+print(Item.pay_rate) # This is class attribute
+print(item1.pay_rate) # This is class attribute they got the value from class level
+print(item2.pay_rate) # This is class attribute they got the value from class level
+
+# print(Item.__dict__) # All the attribute for the class level
+# print(item1.__dict__) # All the attribute for the instance level
+
+item1.apply_discount()
+print(item1.price)
+# item2.apply_discount()
+# print(item2.price)
+
+item2.pay_rate = 0.7
+item2.apply_discount()
+print(item2.price)
