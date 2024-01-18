e3020cc4c490cc8e9189fda8bbc2283560444ee1
Shaikat0079 <baruashaikat00790@gmail.com>
2024-01-18 18:21:23 +0930

    Adding csv contents automatically in the code

--- main.py
+++ main.py
@@ -7,6 +7,8 @@
 # print(type(item1_price)) #int
 # print(type(item1_quality)) #int
 # print(type(item1_price_total)) #int
+import csv
+
 
 class Item:
     pay_rate = 0.8 # The pay rate after 20% dicsount(class attribute)
@@ -34,6 +36,17 @@ class Item:
 
     def apply_discount(self):
         self.price =  self.price * self.pay_rate
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv','r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        
+        for item in items:
+            print(item)
+
 
 
     def __repr__(self) -> str:
@@ -90,20 +103,18 @@ class Item:
 # item2.apply_discount()
 # print(item2.price)
 
-item1 = Item("Phone", 100, 1)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50, 5)
-item5 = Item("Keyboard", 75, 5)
+
 
 
 # print(Item.all)
 
-for instance in Item.all:
-    print(instance.name)
+# for instance in Item.all:
+#     print(instance.name)
 
 
 
 
-print(Item.all)
+# print(Item.all)
+
+Item.instantiate_from_csv()
     
